f2b858254f8b0250e8c621015eb9bde80775ff7f
Jideije Emeka <jideije.emeka@gmail.com>
2021-07-07 08:24:46 -0700

    removed old mail design

--- lib/views/result.dart
+++ lib/views/result.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_maker/views/MailQuiz.dart';
-import 'package:quiz_maker/views/submit_quiz.dart';
 import 'package:quiz_maker/widgets/widgets.dart';
 
 class Results extends StatefulWidget {
